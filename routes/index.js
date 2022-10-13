1f44239d86309d39d85cc73f85a6456ee5cdee27
Arya Chauhan <67852318+chauhanarya007@users.noreply.github.com>
2022-10-13 19:24:51 -0400

    did about and contact form

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Portfolio' });
+  res.render('index', { title: 'Arya Chauhan' });
 });
 
 //Get / about
 router.get('/about', (req, res) => {
   res.render('about', { 
     title: 'About Me',
-    content: 'A short description of me'
+    content: 'A short description of me.'
+  })
+})
+//Get / contact
+router.get('/contact', (req, res) => {
+  res.render('contact', { 
+    title: 'Contact Me',
+    content: 'Contact me here.'
+  })
+})
+//Get / project
+router.get('/projects', (req, res) => {
+  res.render('projects', { 
+    title: 'Projects',
+    content: 'Some Projects made by me.'
   })
 })
 
